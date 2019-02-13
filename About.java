33db259a990e8168d3f63b20ceb3f354af86348a
Darshan Patil <47052204+darshanpatil94@users.noreply.github.com>
2019-02-12 17:56:21 -0800

    Update About.java

--- About.java
+++ About.java
@@ -19,35 +19,12 @@ public class About extends Activity
 		sTextView = (TextView)findViewById(R.id.textView1);
 		str1="Gesture based bluetooth controlled music player android app "
 				+ "developed by E&C department 5th sem students";
-		str2='\n'+ "Darshan," + " Kamran,"+" Ajay"+" and  Akhil. "+'\n';
+		str2='\n'+ "Darshan," + " Kamran,"+" Ajay"+" and  Akhil "+'\n';
 		str3="Under the guidance of Kiran Sir.";
 		
 		sTextView.setText("About:" + '\n'+'\n' +"MusicPlayer v1.0"
 		+ '\n'+'\n'+'\n' + str1 + str2 +'\n'+'\n'+ str3);	
 	}
-
-	/*
-	@Override
-	protected void onStart() 
-	{
-		super.onStart();
-		sTextView = (TextView)findViewById(R.id.textView1);
-		str1="This is an bluetooth controlled android app "
-				+ "developed by EC department 5th sem students;";
-		str2="Darshan Kamran Ajay Akhil. ";
-		sTextView.setText("About:" + str1 + str2); 
-	}
-	
-	@Override
-	protected void onResume() 
-	{
-		super.onResume();
-		/*sTextView = (TextView)findViewById(R.id.textView1);
-		str1="This is an bluetooth controlled android app developed by EC department 5th sem students;";
-		str2='\n'+ " Darshan" + '\n'+ " Kamran"+'\n' + "Ajay"+ '\n' + "Akhil";
-		sTextView.setText("About:"+ '\n' + str1 + str2);
-		
-	}*/
 
 	@Override
 	public void onBackPressed() 
